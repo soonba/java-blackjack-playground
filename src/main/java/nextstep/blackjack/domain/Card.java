5c39a15b2d9bc2e0e5780e8eb6cf1c63fe1189f0
sunba <sunba2318@gmail.com>
2023-04-28 20:49:32 +0900

    mod/test : card, card test

--- src/main/java/nextstep/blackjack/domain/Card.java
+++ src/main/java/nextstep/blackjack/domain/Card.java
@@ -1,10 +1,15 @@
 package nextstep.blackjack.domain;
 
+import nextstep.blackjack.constants.Shape;
+import nextstep.blackjack.constants.StringCard;
+
 import java.security.InvalidParameterException;
 
 public class Card {
     private final Shape shape;
     private final int value;
+    public static final int MIN_CARD_VALUE = 1;
+    public static final int MAX_CARD_VALUE = 13;
 
     public Card(Shape shape, int number) {
         this.shape = shape;
@@ -12,7 +17,7 @@ public class Card {
     }
 
     private int validation(int number) {
-        if(number > 0 && number < 14) return number;
+        if (number >= MIN_CARD_VALUE && number <= MAX_CARD_VALUE) return number;
         throw new InvalidParameterException("value 는 1~13 사이");
     }
 
@@ -22,5 +27,15 @@ public class Card {
 
     public int getValue() {
         return value;
+    }
+
+    public String getString() {
+        String strValue = convertValue(this.value);
+        String strShape = shape.getName();
+        return strValue + strShape;
+    }
+
+    private String convertValue(int value) {
+        return StringCard.numberValueOf(value).orElse(String.valueOf(value));
     }
 }
--- src/test/java/nextstep/blackjack/domain/CardTest.java
+++ src/test/java/nextstep/blackjack/domain/CardTest.java
@@ -1,16 +1,35 @@
 package nextstep.blackjack.domain;
 
-import org.assertj.core.api.Assertions;
+import nextstep.blackjack.constants.Shape;
 import org.junit.jupiter.api.Test;
 
 import java.security.InvalidParameterException;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class CardTest {
 
     @Test
     void 카드번호는_1에서_13사이() {
-        Assertions.assertThatThrownBy(
+        assertThatThrownBy(
                 () -> {Card card = new Card(Shape.CLOVER,14);}
         ).isInstanceOf(InvalidParameterException.class);
+    }
+
+    @Test
+    void 카드_문자변환1() {
+        Card card = new Card(Shape.HEART,13);
+        Card card2 = new Card(Shape.SPADE,1);
+        String str = card.getString();
+        String str2 = card2.getString();
+        assertThat(str).isEqualTo("K하트");
+        assertThat(str2).isEqualTo("A스페이드");
+    }
+
+    @Test
+    void 카드_문자변환2() {
+        Card card = new Card(Shape.HEART,8);
+        String str = card.getString();
+        assertThat(str).isEqualTo("8하트");
     }
 }
