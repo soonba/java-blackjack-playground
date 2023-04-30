9ad6235db9fa6d9a5f131a4d0483751b1e1b2e91
sunba <sunba2318@gmail.com>
2023-04-30 21:04:10 +0900

    test : 금액 계산 Money 불변 클래스로 변경에 따른 테스트코드 변경

--- src/test/java/nextstep/blackjack/domain/MoneyTest.java
+++ src/test/java/nextstep/blackjack/domain/MoneyTest.java
@@ -9,13 +9,13 @@ public class MoneyTest {
     @Test
     void 돈_증가_감소() {
         Money init = Money.init();
-        Money increment = init.increment(10000);
+        Money increment = init.increment(new Money(10000));
         Assertions.assertThat(increment.getValue()).isEqualTo(10000);
 
-        Money decrement = increment.decrement(3000);
+        Money decrement = increment.decrement(new Money(3000));
         Assertions.assertThat(decrement.getValue()).isEqualTo(7000);
 
-        Money decrement2 = decrement.decrement(8000);
+        Money decrement2 = decrement.decrement(new Money(8000));
         Assertions.assertThat(decrement2.getValue()).isEqualTo(-1000);
 
     }
